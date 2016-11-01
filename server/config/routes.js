5579f319a9d9bb2d0a24ba15142f8a9db5454fae
Iliyan Yordanov <iliandj@gmail.com>
2016-11-01 20:23:51 +0200

    New page for 404 error

--- server/config/routes.js
+++ server/config/routes.js
@@ -9,9 +9,15 @@ module.exports = (app) => {
   app.post('/users/create', controllers.users.create)
   app.get('/users/login', controllers.users.login)
   app.post('/users/authenticate', controllers.users.authenticate)
-  app.post('/users/logout', controllers.users.logout)
+  app.post('/users/logout', auth.isAuthenticated, controllers.users.logout)
+  app.get('/users/profile', auth.isAuthenticated, controllers.users.profile)
+  app.post('/users/update', auth.isAuthenticated, controllers.users.update)
+  app.get('/movies/manage', auth.isAuthenticated, controllers.movies.manage)
+  app.get('/movies/add', auth.isAuthenticated, controllers.movies.add)
+  app.post('/movies/insert', auth.isAuthenticated, controllers.movies.insert)
+  // app.get('/categories/manage', auth.isAuthenticated, controllers.categories.manage)
 
-// TODO: attach /users//login?returnUrl=..... to return after login
+// TODO: attach /users/login?returnUrl=..... to return after login
 
 // TODO: To catch all routes with this logic
 //   app.all('/:controller/:method/:id', (req, res) => {
@@ -20,7 +26,7 @@ module.exports = (app) => {
 
   app.all('*', (req, res) => {
     res.status(404)
-    res.send('Not Found')
+    res.render('shared/404')
     res.end()
   })
 }
