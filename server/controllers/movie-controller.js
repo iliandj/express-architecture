26da6073bf8f8b1defab67452cdc2fc708dea92c
Iliyan Yordanov <iliandj@gmail.com>
2016-11-01 20:24:38 +0200

    Add movies route and controller

--- server/controllers/movie-controller.js
+++ server/controllers/movie-controller.js
@@ -1,2 +1,15 @@
 let Movie = require('mongoose').model('Movie')
 
+module.exports = {
+  manage: (req, res) => {
+    res.render('movies/manage')
+  },
+  add: (req, res) => {
+    res.render('movies/add')
+  },
+  insert: (req, res) => {
+    let movie = req.body
+
+     
+  }
+}
